bd14ab346a67129d684148314c19f0342ed93f02
Nitintembhare29 <115885821+Nitintembhare29@users.noreply.github.com>
2023-04-07 12:33:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function sayHello() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+const outputElement = document.querySelector("#output");
+
+sayHello().then((message) => {
+  outputElement.textContent = message;
+});
